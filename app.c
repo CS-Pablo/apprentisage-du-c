457efebf5dd1ca01314c7d6a1203eff4aff926eb
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-01-06 13:30:57 +0100

    Update app.c

---
+++ app.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    // Déclaration et affectation d'une variable de type INT (entier)
+    int aireDuCarre = 0;
+    int aireDuRectangle = 10;
+    // Déclaration et affectation d'une variable de type FLOAT  (nombre a virgule)
+    float prixDeReservation = 125.99;
+    const float PI = 3.14;
+    const float TVA = 20;
+
+    /*
+    
+    %d = nombre entier (int)
+    %f = nombre floatant (float)
+    %c = caractere
+    %s = chaine de caractere (text)
+    
+    */
+    scanf('')
+
+    return 0;
+
+    printf("%d\n", aireDuCarre);
+    printf("l'aire du rectangle est : %d\n", aireDuRectangle),
+    printf('%.2f');
+}
+
