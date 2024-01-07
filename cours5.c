a71cafb5ef6c0d55d215d5c6fb9827e39b407d94
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-01-07 10:40:08 +0100

    Update cours5.c

---
+++ cours5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    int calcul = 6 + 4;
+
+    printf("%d", calcul);
+
+    int nb1 = 6;
+    int nb2 = 3;
+
+    printf("%d", nb1 + nb2);
+
+    return 0;
+}
